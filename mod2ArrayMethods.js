58c76b7fb251acb7d34435792d4a4082f752bcf0
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-07-26 11:15:46 -0400

    object destructuring

--- mod2ArrayMethods.js
+++ mod2ArrayMethods.js
@@ -1,3 +1,19 @@
 const fullName = "German Fabricio Policarpo"
 console.log(fullName.startsWith('German'))
-console.log(fullName.endsWith('Fabricio'))
+console.log(fullName.endsWith('Policarpo'))
+console.log(fullName.startsWith('Fabricio', 7))
+
+// Object destructuring
+
+let dog = {
+    name: "Chloe Antonio",
+    age: 2,
+    favoriteFood: 'Poultry',
+    favoriteShow: "Dog tv"
+}
+
+let { name, age, favoriteFood, favoriteShow } = dog;
+
+console.log(`hello my name is ${name}, I am ${age} years old, my favorite food is  ${favoriteFood} and I love watching 
+${favoriteShow}
+`)
