6f6aa4494d868a40e85352521456bd0bbf453092
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-08-20 20:10:48 -0400

    new

--- friendOrFoe.js
+++ friendOrFoe.js
@@ -1,3 +1,6 @@
+// IF ITEM INARRAY IS EXACTLY FULL CHARS IT WILL PUSH IT TO THE NEW ARRAY
+
+
 function friend(friends) {
     let friendArray = []
     for (let i = 0; i < friends.length; i++) {
---
+++ validatePin.js
@@ -0,0 +1,17 @@
+function validatePIN(pin) {
+    if (pin.length !== 4 && pin.length !== 6) {
+        return false;
+    }
+    for (let i = 0; i < pin.length; i++) {
+        if (pin[i] > '9' || pin[i] < '0') {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+// Clever
+function validatePIN(pin) {
+    return (pin.length == 4 || pin.length == 6) && parseInt(pin) == pin
+}
